fb85d49351da7a850b0cf69ebeddd65866650290
dan2077 <dan2077x@gmail.com>
2015-06-24 13:33:07 -0500

    Filter fileName by .pk3 and readme.txt

--- src/DataManager.java
+++ src/DataManager.java
@@ -93,23 +93,27 @@ public class DataManager {
 	    	while(ze!=null){
 	 
 	    	   String fileName = ze.getName();
-	           File newFile = new File(outputFolder + File.separator + fileName);
-	 
-	           System.out.println("file unzip : "+ newFile.getAbsoluteFile());
-	 
-	            //create all non exists folders
-	            //else you will hit FileNotFoundException for compressed folder
-	            new File(newFile.getParent()).mkdirs();
-	 
-	            FileOutputStream fos = new FileOutputStream(newFile);             
-	 
-	            int len;
-	            while ((len = zis.read(buffer)) > 0) {
-	       		fos.write(buffer, 0, len);
-	            }
-	 
-	            fos.close();   
-	            ze = zis.getNextEntry();
+	    	   
+	    	   if ( fileName.contains(".pk3") || fileName.contains("readme.txt")) {
+		           File newFile = new File(outputFolder + File.separator + fileName);
+		 
+		           System.out.println("file unzip : "+ newFile.getAbsoluteFile());
+		 
+		            //create all non exists folders
+		            //else you will hit FileNotFoundException for compressed folder
+		            new File(newFile.getParent()).mkdirs();
+		 
+		            FileOutputStream fos = new FileOutputStream(newFile);             
+		 
+		            int len;
+		            while ((len = zis.read(buffer)) > 0) {
+		       		fos.write(buffer, 0, len);
+		            }
+		 
+		            fos.close();
+	    	   }
+	    	   
+	           ze = zis.getNextEntry();
 	    	}
 	 
 	        zis.closeEntry();
